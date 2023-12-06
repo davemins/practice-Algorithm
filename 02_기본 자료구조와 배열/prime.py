aadb66837ccb49b0bd448e059a8c49e9361322f5
davemins <davemins4125@naver.com>
2023-12-06 21:09:09 +0900

    feat: 1,000 이하의 소수를 나열하기

--- 02_기본 자료구조와 배열/prime.py
+++ 02_기본 자료구조와 배열/prime.py
@@ -7,15 +7,20 @@ prime = [None] * 500
 prime[ptr] = 2
 ptr += 1
 
-for n in range(3, 1001, 2):
-    for i in range(1, ptr):
-        counter += 1
+prime[ptr] = 3
+ptr += 1
+
+for n in range(5, 1001, 2):
+    i = 1
+    while prime[i] * prime[i] <= n:
+        counter += 2
         if n % prime[i] == 0:
             break
-
+        i += 1
     else:
         prime[ptr] = n
         ptr += 1
+        counter += 1
 
 for i in range(ptr):
     print(prime[i])
