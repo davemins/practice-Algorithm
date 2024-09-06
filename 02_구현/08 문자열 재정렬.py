e0be0321dd35bcd7eec324a37ce81164f7703352
davemins <davemins4125@naver.com>
2024-09-07 08:52:58 +0900

    문자열 재정렬

--- 02_구현/08 문자열 재정렬.py
+++ 02_구현/08 문자열 재정렬.py
@@ -1,20 +1,47 @@
-input_data = input()
-output_data = ''
+'''
+# 문제 이해
+대문자와 숫자를 분리한 후 대문자는 모두 오름차순 정렬하고, 숫자는 모두 더하여 둘을 이어 출력한다.
 
-num_list = ['0', '1', '2', '3', '4', '5', '6', '7', ' 8', '9']
+# 발상
+이것도 완전탐색 문제이다.
 
-input_str = []
-sum = 0
+# 복잡도
+입력 문자열의 길이를 n이라고 할 때 O(n)의 시간복잡도를 가진다.
 
-for i in input_data:
-    if i in num_list:
-        sum += int(i)
+'''
+data = input()
+
+alphabets = []
+alphabet = ''
+numbers = []
+
+number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+for i in range(len(data)):
+    if data[i] in number_list:
+        numbers.append(data[i])
     else:
-        input_str.append(i)
+        alphabets.append(data[i])
 
-input_str.sort()
+alphabets.sort()
 
-for j in input_str:
-    output_data += j
+for a in range(len(alphabets)):
+    alphabet += alphabets[a]
 
-print(output_data + str(sum))
+if len(numbers) != 0:
+    result = 0
+    for j in range(len(numbers)):
+        result += int(numbers[j])
+    print(alphabet + str(result))
+else:
+    print(alphabet)
+'''
+# 푼 시간
+14분
+
+# 채점
+정답
+
+# 느낀 점
+확실히 구현의 난이도 낮은 문제는 빨리 풀 수 있다.
+'''
