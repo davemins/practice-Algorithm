b5c0ad6ed366bd8eec4ee0094669ecfdb0f3be94
davemins <davemins4125@naver.com>
2024-10-14 21:40:06 +0900

    재귀 함수로 구현된 이진 탐색

---
+++ Archive/탐색 이진 (재귀 함수).py
@@ -0,0 +1,44 @@
+'''
+<재귀 함수로 구현된 이진 탐색>
+
+# 입력
+
+
+# 출력
+
+
+'''
+# 이진 탐색 소스코드 구현(재귀 함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력 받기
+n, target = list(map(int, input().split()))
+
+# 전체 원소 입력 받기
+array = list(map(int, input().split()))
+
+# 이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
+'''
+# 생각
+그렇게 어렵지는 않다.
+조건문으로 값이 나올 때까지 재귀 함수 호출하면 될 것 같다.
+
+'''
