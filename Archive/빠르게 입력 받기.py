830b5b84d752e0bd3900cfb22c3145da9468c23b
davemins <davemins4125@naver.com>
2024-10-17 09:40:55 +0900

    빠르게 입력 받기

---
+++ Archive/빠르게 입력 받기.py
@@ -0,0 +1,24 @@
+'''
+<빠르게 입력 받기>
+
+# 입력
+
+
+# 출력
+
+
+'''
+import sys
+
+# 하나의 문자열 데이터 입력 받기
+input_data = sys.stdin.readline().rstrip()
+
+# 입력 받은 문자열 그대로 출력
+print(input_data)
+
+
+'''
+# 생각
+
+
+'''
